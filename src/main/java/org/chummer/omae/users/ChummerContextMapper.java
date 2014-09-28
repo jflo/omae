89ebb77b3771ab3676c3d1b09c3732f2a21d30df
jflo <jflorentine@gmail.com>
2014-09-28 18:31:59 -0400

    forgetten adds

---
+++ src/main/java/org/chummer/omae/users/ChummerContextMapper.java
@@ -0,0 +1,18 @@
+package org.chummer.omae.users;
+
+import org.springframework.ldap.core.ContextMapper;
+import org.springframework.ldap.core.DirContextAdapter;
+
+public class ChummerContextMapper implements ContextMapper<Chummer> {
+	
+	public Chummer mapFromContext(Object ctx) {
+		DirContextAdapter context = (DirContextAdapter) ctx;
+		Chummer c = new Chummer();
+		c.setUserName(context.getStringAttribute("cn"));
+		c.setEmail(context.getStringAttribute("mail"));
+
+		return c;
+	}
+	
+	
+}
